eeb0e26db75216c642f43632e50b9e32db664000
Farhan Azam <fazam07idea@gmail.com>
2026-04-02 17:56:15 +0530

    close to final

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -7,6 +7,20 @@ import gallery6 from "@/assets/gallery-6.jpg";
 import gallery7 from "@/assets/gallery-7.jpg";
 import gallery8 from "@/assets/gallery-8.jpg";
 
+import basketball1 from "@/assets/basketball/basket-ball-1.jpg";
+import basketball2 from "@/assets/basketball/basket-ball-2.jpg";
+import basketball3 from "@/assets/basketball/basket-ball-3.jpg";
+import basketball4 from "@/assets/basketball/basket-ball-4.jpg";
+import basketball5 from "@/assets/basketball/basket-ball-5.jpg";
+import basketball6 from "@/assets/basketball/basket-ball-6.jpg";
+
+import kabaddi1 from "@/assets/kabaddi/kabaddi-1.jpg";
+import kabaddi2 from "@/assets/kabaddi/kabaddi-2.jpg";
+
+import tbltns1 from "@/assets/table tennis/table-tennis-1.jpg";
+import tbltns2 from "@/assets/table tennis/table-tennis-2.jpg";
+
+
 export interface ProgramData {
   id: string;
   name: string;
@@ -40,32 +54,34 @@ export const galleryData: YearData[] = [
         id: "basket-ball",
         name: "Basket Ball",
         icon: "Sparkles",
-        cover: gallery1,
+        cover: basketball5,
         media: [
-          { src: gallery1, type: "image", title: "" },
-          { src: gallery5, type: "image", title: "" },
-          { src: gallery6, type: "image", title: "" },
+          { src: basketball1, type: "image", title: "" },
+          { src: basketball2, type: "image", title: "" },
+          { src: basketball3, type: "image", title: "" },
+          { src: basketball4, type: "image", title: "" },
+          { src: basketball5, type: "image", title: "" },
+          { src: basketball6, type: "image", title: "" },
         ],
       },
       {
         id: "architecture-tour",
-        name: "Architecture Tour",
+        name: "Kabaddi",
         icon: "Building2",
-        cover: gallery2,
+        cover: kabaddi1,
         media: [
-          { src: gallery2, type: "image", title: "" },
-          { src: gallery6, type: "image", title: "" },
+          { src: kabaddi1, type: "image", title: "" },
+          { src: kabaddi2, type: "image", title: "" },
         ],
       },
       {
         id: "ocean-retreat",
-        name: "Ocean Retreat",
+        name: "Table Tennis",
         icon: "Waves",
-        cover: gallery5,
+        cover: tbltns1,
         media: [
-          { src: gallery5, type: "image", title: "" },
-          { src: gallery7, type: "image", title: "" },
-          { src: gallery1, type: "image", title: "" },
+          { src: tbltns1, type: "image", title: "" },
+          { src: tbltns2, type: "image", title: "" }
         ],
       },
       {
