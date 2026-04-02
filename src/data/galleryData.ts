6cb8a9a2448567648258a85505624db71fbd204a
Farhan Azam <fazam07idea@gmail.com>
2026-04-02 19:18:52 +0530

    close to final

--- src/data/galleryData.ts
+++ src/data/galleryData.ts
@@ -7,18 +7,31 @@ import gallery6 from "@/assets/gallery-6.jpg";
 import gallery7 from "@/assets/gallery-7.jpg";
 import gallery8 from "@/assets/gallery-8.jpg";
 
+
+import bad1 from "@/assets/badminton/badminton-1.jpg";
+import bad2 from "@/assets/badminton/badminton-2.jpeg";
+
 import basketball1 from "@/assets/basketball/basket-ball-1.jpg";
 import basketball2 from "@/assets/basketball/basket-ball-2.jpg";
 import basketball3 from "@/assets/basketball/basket-ball-3.jpg";
 import basketball4 from "@/assets/basketball/basket-ball-4.jpg";
 import basketball5 from "@/assets/basketball/basket-ball-5.jpg";
 import basketball6 from "@/assets/basketball/basket-ball-6.jpg";
+import basketball7 from "@/assets/basketball/basket-ball-7.jpeg";
+
+
+import chess from "@/assets/chess/chess-1.jpg";
+
+
 
 import kabaddi1 from "@/assets/kabaddi/kabaddi-1.jpg";
 import kabaddi2 from "@/assets/kabaddi/kabaddi-2.jpg";
 
 import tbltns1 from "@/assets/table tennis/table-tennis-1.jpg";
 import tbltns2 from "@/assets/table tennis/table-tennis-2.jpg";
+import tbltns3 from "@/assets/table tennis/table-tennis-3.jpeg";
+
+
 
 
 export interface ProgramData {
@@ -39,15 +52,13 @@ export const galleryData: YearData[] = [
     year: "2026",
     programs: [
       {
-        id: "cricket",
-        name: "Cricket",
-        icon: "TreePine",
-        cover: gallery4,
+        id: "badminton",
+        name: "Badminton",
+        icon: "Tent",
+        cover: bad1,
         media: [
-          { src: gallery4, type: "image", title: "" },
-          { src: gallery7, type: "image", title: "" },
-          { src: gallery3, type: "image", title: "" },
-          { src: gallery8, type: "image", title: "" },
+          { src: bad1, type: "image", title: "" },
+          { src: bad2, type: "image", title: "" },
         ],
       },
       {
@@ -62,10 +73,33 @@ export const galleryData: YearData[] = [
           { src: basketball4, type: "image", title: "" },
           { src: basketball5, type: "image", title: "" },
           { src: basketball6, type: "image", title: "" },
+          { src: basketball7, type: "image", title: "" },
         ],
       },
       {
-        id: "architecture-tour",
+        id: "chess",
+        name: "Chess",
+        icon: "MapPin",
+        cover: chess,
+        media: [
+          { src: chess, type: "image", title: "" },
+        ],
+      },
+      {
+        id: "cricket",
+        name: "Cricket",
+        icon: "TreePine",
+        cover: gallery4,
+        media: [
+          { src: gallery4, type: "image", title: "" },
+          { src: gallery7, type: "image", title: "" },
+          { src: gallery3, type: "image", title: "" },
+          { src: gallery8, type: "image", title: "" },
+        ],
+      },
+
+      {
+        id: "kabaddi",
         name: "Kabaddi",
         icon: "Building2",
         cover: kabaddi1,
@@ -75,46 +109,14 @@ export const galleryData: YearData[] = [
         ],
       },
       {
-        id: "ocean-retreat",
+        id: "table-tennis",
         name: "Table Tennis",
         icon: "Waves",
         cover: tbltns1,
         media: [
           { src: tbltns1, type: "image", title: "" },
-          { src: tbltns2, type: "image", title: "" }
-        ],
-      },
-      {
-        id: "street-stories",
-        name: "Street Stories",
-        icon: "MapPin",
-        cover: gallery6,
-        media: [
-          { src: gallery6, type: "image", title: "" },
-          { src: gallery2, type: "image", title: "" },
-          { src: gallery8, type: "image", title: "" },
-        ],
-      },
-      {
-        id: "wilderness-camp",
-        name: "Fire Camp",
-        icon: "Tent",
-        cover: gallery8,
-        media: [
-          { src: gallery8, type: "image", title: "" },
-          { src: gallery3, type: "image", title: "" },
-          { src: gallery4, type: "image", title: "" },
-        ],
-      },
-      {
-        id: "wilderness-camp",
-        name: "CAMP",
-        icon: "Tent",
-        cover: gallery8,
-        media: [
-          { src: gallery8, type: "image", title: "" },
-          { src: gallery3, type: "image", title: "" },
-          { src: gallery4, type: "image", title: "" },
+          { src: tbltns2, type: "image", title: "" },
+          { src: tbltns3, type: "image", title: "" }
         ],
       },
     ],
